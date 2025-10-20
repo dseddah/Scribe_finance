a03e509d92a781e4d6a31eae15cfe2b813c0262b
SEDDAH Djame <djame.seddah@inria.fr>
2025-10-20 21:44:50 +0200

    updating finance dataset

--- run_me.sh
+++ run_me.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-python excel_to_json.py --in Q\&A_finance_and_others\ \(beta\ version\).20250422-1601.xlsx --out dataset_json --source_data_dir raw_documents/
+#python excel_to_json.py --in Q\&A_finance_and_others\ \(beta\ version\).20250422-1601.xlsx --out dataset_json --source_data_dir raw_documents/
+
+python excel_to_json.py --in Q\&A_finance_and_others\ \(beta\ version\).20251020-2141.xlsx --out dataset_json_20251020-2141 --source_data_dir raw_documents/
+
