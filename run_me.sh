2c04c5c26c7064eb283ae992082b3b0f7166173b
SEDDAH Djame <djame.seddah@inria.fr>
2025-10-21 11:03:49 +0200

    new version avec id 1ere colummn

--- run_me.sh
+++ run_me.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
+# 1st version
 #python excel_to_json.py --in Q\&A_finance_and_others\ \(beta\ version\).20250422-1601.xlsx --out dataset_json --source_data_dir raw_documents/
 
-python excel_to_json.py --in Q\&A_finance_and_others\ \(beta\ version\).20251020-2141.xlsx --out dataset_json_20251020-2141 --source_data_dir raw_documents/
+
+# 2snd version
+#python excel_to_json.py --in Q\&A_finance_and_others\ \(beta\ version\).20251020-2141.xlsx --out dataset_json_20251020-2141 --source_data_dir raw_documents/
+
+
+# 3rd version (avec id)
+
+FILEIN='Q\&A_finance_and_others\ \(beta\ version\).20251021-1055.xlsx'
+#ls $FILEIN
+python excel_to_json.py --in Q\&A_finance_and_others\ \(beta\ version\).20251021-1055.xlsx  --out dataset_json_20251021-1055 --source_data_dir raw_documents/
 
